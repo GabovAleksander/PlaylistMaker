d837532bf38c1c1841e4a28775e1300eaf8ed8a5
GabovAleksander <avlagab@yandex.ru>
2024-03-04 09:49:31 +0300

    Домашнее задание спринт 12

---
+++ app/src/main/java/com/practicum/playlistmaker/SearchHistory.kt
@@ -0,0 +1,53 @@
+package com.practicum.playlistmaker
+
+import android.content.SharedPreferences
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
+class SearchHistory(val preferences: SharedPreferences) {
+    private val savedList = preferences.getString(HISTORY_KEY, "[]")
+
+    class Token : TypeToken<MutableList<Track>>()
+
+    private val list: MutableList<Track> =
+        if (savedList == null) mutableListOf() else Gson().fromJson<ArrayList<Track>>(savedList, Token().type).toMutableList()
+
+    fun loadHistory(): MutableList<Track> {
+        return list.reversed().toMutableList()
+    }
+
+    fun saveTrack(track: Track) {
+        var isContains = false
+        for (i in list) {
+            if (track.trackId.equals(i.trackId)) {
+                list.remove(i)
+                list.add(i)
+                isContains = true
+                break
+            }
+        }
+        if (!isContains) {
+            list.add(track)
+            if (list.count() > HISTORY_SIZE) {
+                list.removeAt(0)
+            }
+        }
+        val addJSon = Gson().toJson(list)
+        preferences.edit()
+            .putString(HISTORY_KEY, addJSon)
+            .apply()
+    }
+
+    fun clearHistory() {
+        list.clear()
+        val emptyJSon = Gson().toJson(list)
+        preferences.edit()
+            .putString(HISTORY_KEY, emptyJSon)
+            .apply()
+    }
+
+    companion object {
+        private const val HISTORY_KEY = "history"
+        private const val HISTORY_SIZE = 10
+    }
+}
--- app/src/main/java/com/practicum/playlistmaker/TrackListAdapter.kt
+++ app/src/main/java/com/practicum/playlistmaker/TrackListAdapter.kt
@@ -1,10 +1,13 @@
 package com.practicum.playlistmaker
 
+import android.content.SharedPreferences
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class TrackListAdapter(var items: ArrayList<Track>) :
+class TrackListAdapter(var items: MutableList<Track>, sharedPreferences: SharedPreferences) :
     RecyclerView.Adapter<TrackListViewHolder>() {
+
+    val searchHistory = SearchHistory(sharedPreferences)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackListViewHolder = TrackListViewHolder(parent)
 
@@ -12,6 +15,9 @@ class TrackListAdapter(var items: ArrayList<Track>) :
 
     override fun onBindViewHolder(holder: TrackListViewHolder, position: Int) {
         holder.bind(items.get(position))
+        holder.itemView.setOnClickListener{
+            searchHistory.saveTrack(items.get(position))
+        }
     }
 
 }
