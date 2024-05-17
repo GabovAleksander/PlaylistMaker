9c3ee8b75841faf5155dcdb6a7d9e5a68b441115
GabovAleksander <avlagab@yandex.ru>
2024-05-17 17:30:27 +0300

    Домашнее задание спринт 15_2

--- app/src/main/java/com/practicum/playlistmaker/Resource.kt
+++ app/src/main/java/com/practicum/playlistmaker/Resource.kt
@@ -1,6 +1,6 @@
 package com.practicum.playlistmaker
 
-sealed class Resource<T> (val data: T? = null, val message: String? = null, val code: Int){
+sealed class Resource<T> (val data: T? = null, val code: Int, val message: String? = null){
     class Success<T>(data: T, code: Int): Resource<T>(data, code = code)
-    class Error<T>(message: String, data: T? = null, code: Int): Resource<T>(data, message, code)
+    class Error<T>(data: T? = null, code: Int, message: String): Resource<T>(data, code, message)
 }
